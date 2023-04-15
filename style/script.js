cf5a2b658c79abdcc3777c959a273eb9daa530ea
David Enéas <david00eneas@gmail.com>
2023-04-15 16:49:44 -0300

    melhoria na interface da area do resultado, correção na função copia

--- style/script.js
+++ style/script.js
@@ -1,14 +1,17 @@
-let textInput = document.getElementById('text');
-let copyButton = document.getElementById('copy');
 let buttonC = document.getElementById('cpt');
 let buttonD = document.getElementById('dcpt');
+let divpalceHolder =document.querySelector('.resultBox');
+let funcaoUso = false;
 
+function mudaTela(){
+	divpalceHolder.innerHTML = "<div class='placeholder'><button id='copy' style='display:none'></button><h2>Nenhuma mensagem encontrada</h2><br><h3>Digite um texto que você deseja criptografar ou descriptografar.</h3></div>";
+}
 let input = document.querySelector("#entrada");
 
 	function validarInput(letranEntrada) {
 		if(!chegaLetra(letranEntrada)) {
 			letranEntrada.preventDefault();
-	 	 alert("Evite digitar letras maiúsculas ou caracteres especiais!");
+	 	 	alert("Evite digitar letras maiúsculas ou caracteres especiais!");
 		}
   	}
 
@@ -21,41 +24,54 @@ let input = document.querySelector("#entrada");
 	}
 
 	function criptografa(){	
-		let mensagem=[];
 		let textoE =document.getElementById('entrada').value;
-		codificando=Array.from(textoE);
-		for (letra in codificando){
-			if(codificando[letra]=="a"){
-				mensagem.push("ai");
-			}else if(codificando[letra]=="e"){
-				mensagem.push("enter");
-			}else if(codificando[letra]=="i"){
-				mensagem.push("imes");
-			}else if(codificando[letra]=="o"){
-				mensagem.push("ober");
-			}else if(codificando[letra]=="u"){
-				mensagem.push("ufat");
-			}else{
-				mensagem.push(codificando[letra]);
+		if(textoE!=""){
+			let mensagem=[];
+			codificando=Array.from(textoE);
+			for (letra in codificando){
+				if(codificando[letra]=="a"){
+					mensagem.push("ai");
+				}else if(codificando[letra]=="e"){
+					mensagem.push("enter");
+				}else if(codificando[letra]=="i"){
+					mensagem.push("imes");
+				}else if(codificando[letra]=="o"){
+					mensagem.push("ober");
+				}else if(codificando[letra]=="u"){
+					mensagem.push("ufat");
+				}else{
+					mensagem.push(codificando[letra]);
+				}
 			}
+			divpalceHolder.innerHTML ='<textarea id="text" class="resultArea" readonly></textarea><button class="buttons btCopy" id="copy">Copiar</button>';
+			document.getElementById('text').value=mensagem.join("");
+			document.getElementById('copy').onclick = copiaTexto;
+		}else{
+			mudaTela();
 		}
-				textInput.value=mensagem.join("");
 	}
-		
+
 	function descriptografa(){
 		let textoE =document.getElementById('entrada').value;
-		mensagemDescriptografada=textoE.replace(/ai/g,"a");
-		mensagemDescriptografada=mensagemDescriptografada.replace(/enter/g,"e");
-		mensagemDescriptografada=mensagemDescriptografada.replace(/imes/g,"i");
-		mensagemDescriptografada=mensagemDescriptografada.replace(/ober/g,"o");
-		mensagemDescriptografada=mensagemDescriptografada.replace(/ufat/g,"u");
-			textInput.value=mensagemDescriptografada;		 
+		if(textoE!=""){
+			mensagemDescriptografada=textoE.replace(/ai/g,"a");
+			mensagemDescriptografada=mensagemDescriptografada.replace(/enter/g,"e");
+			mensagemDescriptografada=mensagemDescriptografada.replace(/imes/g,"i");
+			mensagemDescriptografada=mensagemDescriptografada.replace(/ober/g,"o");
+			mensagemDescriptografada=mensagemDescriptografada.replace(/ufat/g,"u");
+			divpalceHolder.innerHTML ='<textarea id="text" class="resultArea" readonly></textarea><button class="buttons btCopy" id="copy">Copiar</button>';
+			document.getElementById('text').value=mensagemDescriptografada;
+			document.getElementById('copy').onclick = copiaTexto;
+		}else{
+			mudaTela();
+		}
 	}
 	function copiaTexto(){
-		textInput.select();
+		document.getElementById('text').select();
 		document.execCommand('copy');
 	}
+
+	mudaTela();
 	input.addEventListener("keypress", validarInput);
-	copyButton.onclick = copiaTexto;
 	buttonC.onclick = criptografa;
-	buttonD.onclick = descriptografa;
+	buttonD.onclick = descriptografa;	
